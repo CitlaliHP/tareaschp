74bce95da725d4758ebda883a2dd73661b17d427
CitlaliHP <122800835+CitlaliHP@users.noreply.github.com>
2023-02-05 22:52:26 +0000

    calcular el área y perimetro de un triangulo

---
+++ semana1/programa1.py
@@ -0,0 +1,15 @@
+"""
+    programa1
+    Nombre: Citlali Hernández Pérez
+    Fecha: 05/02/2023
+    Descripcion:
+    Área y perímetro de un rectangulo
+"""
+print('Área y perímetro de un rectangulo')
+print()
+base = int(input('Ingrese el valor de la base: '))
+altura = int(input('Ingrese el valor de la altura: '))
+area = base * altura
+perimetro = base + altura + base + altura
+print('El area es de: ', area)
+print('El perimetro es de: ', perimetro)
